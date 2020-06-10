a38f66c9901c30219e945ae9157fec9b120b0f9a
JiaJun-Y <729157930@qq.com>
2020-06-10 18:03:16 +0800

    'home'

--- single/common/header.js
+++ single/common/header.js
@@ -6,10 +6,17 @@ function showTime() {
 setInterval("showTime()", "1000");
 
 function _currentTime() {
-  var now = new Date();
-  var _month = (10 > (now.getMonth() + 1)) ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
-  var _day = (10 > now.getDate()) ? '0' + now.getDate() : now.getDate();
-  var _hour = (10 > now.getHours()) ? '0' + now.getHours() : now.getHours();
-  var _minute = (10 > now.getMinutes()) ? '0' + now.getMinutes() : now.getMinutes();
-  return now.getFullYear() + '/' + _month + '/' + _day + ' ' + _hour + ':' + _minute;
+  var date = moment().format("YYYY/MM/DD HH:mm")
+  var week = moment(date).day();
+  return date + '    ' + ' 星期' + DX(week)
+}
+
+function DX(n) {
+  switch (n) {
+    case 3:
+      return '三'
+      break;
+    default:
+      //  默认代码块
+  }
 }
